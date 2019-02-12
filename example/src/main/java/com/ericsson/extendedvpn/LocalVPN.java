229ab2af8eb8bfa24f7edd64eb4f97b10a95f420
FranCarmona <francisco.jose.carmona@blue-tc.com>
2019-02-12 09:42:44 +0100

    Changed to a lib project and example

--- example/src/main/java/com/ericsson/extendedvpn/LocalVPN.java
+++ example/src/main/java/com/ericsson/extendedvpn/LocalVPN.java
@@ -1,18 +1,18 @@
 /*
-** Copyright 2015, Mohamed Naufal
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-*/
+ ** Copyright 2015, Mohamed Naufal
+ **
+ ** Licensed under the Apache License, Version 2.0 (the "License");
+ ** you may not use this file except in compliance with the License.
+ ** You may obtain a copy of the License at
+ **
+ **     http://www.apache.org/licenses/LICENSE-2.0
+ **
+ ** Unless required by applicable law or agreed to in writing, software
+ ** distributed under the License is distributed on an "AS IS" BASIS,
+ ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ** See the License for the specific language governing permissions and
+ ** limitations under the License.
+ */
 
 package com.ericsson.extendedvpn;
 
@@ -22,25 +22,21 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.VpnService;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 
-public class LocalVPN extends ActionBarActivity
-{
+public class LocalVPN extends AppCompatActivity {
     private static final int VPN_REQUEST_CODE = 0x0F;
 
     private boolean waitingForVPNStart;
 
-    private BroadcastReceiver vpnStateReceiver = new BroadcastReceiver()
-    {
+    private BroadcastReceiver vpnStateReceiver = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context context, Intent intent)
-        {
-            if (LocalVPNService.BROADCAST_VPN_STATE.equals(intent.getAction()))
-            {
+        public void onReceive(Context context, Intent intent) {
+            if (LocalVPNService.BROADCAST_VPN_STATE.equals(intent.getAction())) {
                 if (intent.getBooleanExtra("running", false))
                     waitingForVPNStart = false;
             }
@@ -48,16 +44,13 @@ public class LocalVPN extends ActionBarActivity
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_vpn);
-        final Button vpnButton = (Button)findViewById(R.id.vpn);
-        vpnButton.setOnClickListener(new View.OnClickListener()
-        {
+        final Button vpnButton = (Button) findViewById(R.id.vpn);
+        vpnButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 startVPN();
             }
         });
@@ -66,8 +59,7 @@ public class LocalVPN extends ActionBarActivity
                 new IntentFilter(LocalVPNService.BROADCAST_VPN_STATE));
     }
 
-    private void startVPN()
-    {
+    private void startVPN() {
         Intent vpnIntent = VpnService.prepare(this);
         if (vpnIntent != null)
             startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
@@ -76,11 +68,9 @@ public class LocalVPN extends ActionBarActivity
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK)
-        {
+        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
             waitingForVPNStart = true;
             startService(new Intent(this, LocalVPNService.class));
             enableButton(false);
@@ -94,16 +84,12 @@ public class LocalVPN extends ActionBarActivity
         enableButton(!waitingForVPNStart && !LocalVPNService.isRunning());
     }
 
-    private void enableButton(boolean enable)
-    {
+    private void enableButton(boolean enable) {
         final Button vpnButton = (Button) findViewById(R.id.vpn);
-        if (enable)
-        {
+        if (enable) {
             vpnButton.setEnabled(true);
             vpnButton.setText(R.string.start_vpn);
-        }
-        else
-        {
+        } else {
             vpnButton.setEnabled(false);
             vpnButton.setText(R.string.stop_vpn);
         }
