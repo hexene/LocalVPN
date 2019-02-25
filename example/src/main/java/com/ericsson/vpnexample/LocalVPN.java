ba074b6879b88f37a62276fed55791580743361b
FranCarmona <francisco.jose.carmona@blue-tc.com>
2019-02-25 13:51:58 +0100

    Project structure rebuilded: example and extendedvpn modules

--- example/src/main/java/com/ericsson/vpnexample/LocalVPN.java
+++ example/src/main/java/com/ericsson/vpnexample/LocalVPN.java
@@ -14,18 +14,21 @@
  ** limitations under the License.
  */
 
-package com.ericsson.extendedvpn;
+package com.ericsson.vpnexample;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.VpnService;
-import android.support.v4.content.LocalBroadcastManager;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.ericsson.extendedvpn.LocalVPNService;
+import com.ericsson.extendedvpn.R;
 
 
 public class LocalVPN extends AppCompatActivity {
